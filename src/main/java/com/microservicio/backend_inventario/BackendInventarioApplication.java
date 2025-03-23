a49af2117b87b27a03ac832961df6a5f78642db6
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-03-23 18:53:38 -0300

    Creación microservicio de administración de inventario

---
+++ src/main/java/com/microservicio/backend_inventario/BackendInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.backend_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInventarioApplication.class, args);
+	}
+
+}
